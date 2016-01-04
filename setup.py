efaea67583d62654a881ece7972b4687abfdde0f
Christian Scholz <cs@comlounge.net>
2016-01-04 16:23:55 +0100

    updated meta tags and version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='sf-babel',
-    version='0.1-dev',
+    version='1.0',
     url='',
     license='BSD',
     author='Christian Scholz',
@@ -32,7 +32,6 @@ setup(
     ],
     include_package_data=True,
     classifiers=[
-        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
